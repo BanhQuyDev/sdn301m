08e3cfe294957280f762d1ef12655a8c650c90c4
BanhQuyDev <BanhQuyDev>
2023-01-05 11:17:04 +0700

    Express Router

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const morgan = require("morgan");
 app.use(bodyParser.json());
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
+const nationRouter = require('./routes/nationRouter');
+app.use('/nations', nationRouter)
 // app.use((req, res, next) => {
 //   console.log(req.headers);
 //   res.statusCode = 200;
@@ -18,54 +20,54 @@ const server = http.createServer(app);
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
-app.all("/nations", (req, res, next) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  next();
-});
+// app.all("/nations", (req, res, next) => {
+//   res.statusCode = 200;
+//   res.setHeader("Content-Type", "text/plain");
+//   next();
+// });
 
-app.get("/nations", (req, res, next) => {
-  res.end("Will send all the nations to you!");
-});
+// app.get("/nations", (req, res, next) => {
+//   res.end("Will send all the nations to you!");
+// });
 
-app.post("/nations", (req, res, next) => {
-  res.end(
-    "Will add the nation: " +
-      req.body.name +
-      " with details: " +
-      req.body.description
-  );
-});
+// app.post("/nations", (req, res, next) => {
+//   res.end(
+//     "Will add the nation: " +
+//       req.body.name +
+//       " with details: " +
+//       req.body.description
+//   );
+// });
 
-app.put("/nations", (req, res, next) => {
-  res.statusCode = 403;
-  res.end("PUT operation not supported on /nations");
-});
+// app.put("/nations", (req, res, next) => {
+//   res.statusCode = 403;
+//   res.end("PUT operation not supported on /nations");
+// });
 
-app.delete("/nations", (req, res, next) => {
-  res.end("Deleting all nations");
-});
+// app.delete("/nations", (req, res, next) => {
+//   res.end("Deleting all nations");
+// });
 
-app.get("/nations/:nationId", (req, res, next) => {
-  res.end(
-    "Will send details of the nation: " + req.params.nationId + " to you!"
-  );
-});
+// app.get("/nations/:nationId", (req, res, next) => {
+//   res.end(
+//     "Will send details of the nation: " + req.params.nationId + " to you!"
+//   );
+// });
 
-app.post("/nations/:nationId", (req, res, next) => {
-  res.statusCode = 403;
-  res.end("POST operation not supported on /nations/" + req.params.nationId);
-});
+// app.post("/nations/:nationId", (req, res, next) => {
+//   res.statusCode = 403;
+//   res.end("POST operation not supported on /nations/" + req.params.nationId);
+// });
 
-app.put("/nations/:nationId", (req, res, next) => {
-  res.write("Updating the nation: " + req.params.nationId + "\n");
-  res.end(
-    "Will update the nation: " +
-      req.body.name +
-      " with details: " +
-      req.body.description
-  );
-});
-app.delete("/nations/:nationId", (req, res, next) => {
-  res.end("Deleting nation: " + req.params.nationId);
-});
+// app.put("/nations/:nationId", (req, res, next) => {
+//   res.write("Updating the nation: " + req.params.nationId + "\n");
+//   res.end(
+//     "Will update the nation: " +
+//       req.body.name +
+//       " with details: " +
+//       req.body.description
+//   );
+// });
+// app.delete("/nations/:nationId", (req, res, next) => {
+//   res.end("Deleting nation: " + req.params.nationId);
+// });
